90ae265b8f06cd2423062c83abdb69a9595d9623
w8zrd <ankitbungaliya@gmail.com>
2025-10-27 04:48:19 -0400

    app.js commit 8

--- app.js
+++ app.js
@@ -2,6 +2,45 @@
    iPing - app.js
    ======================= */
 
+import { initializeApp } from "firebase/app";
+import { getAI, getGenerativeModel, GoogleAIBackend } from "firebase/ai";
+
+// TODO(developer) Replace the following with your app's Firebase configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+const firebaseConfig = {
+  apiKey: "AIzaSyCq_XEdRwe1lRA7y2FfljFmfa5n-zQWJSw",
+  authDomain: "ipingonline.firebaseapp.com",
+  projectId: "ipingonline",
+  storageBucket: "ipingonline.firebasestorage.app",
+  messagingSenderId: "880672073705",
+  appId: "1:880672073705:web:d1bce5f5b3f8cf001a4d78",
+  measurementId: "G-LN4GBVV603"
+};
+
+// Initialize FirebaseApp
+const firebaseApp = initializeApp(firebaseConfig);
+
+// Initialize the Gemini Developer API backend service
+const ai = getAI(firebaseApp, { backend: new GoogleAIBackend() });
+
+// Create a `GenerativeModel` instance with a model that supports your use case
+const model = getGenerativeModel(ai, { model: "gemini-2.5-flash" });
+
+// Wrap in an async function so you can use await
+async function run() {
+  // Provide a prompt that contains text
+  const prompt = "Write a story about a magic backpack."
+
+  // To generate text output, call generateContent with the text input
+  const result = await model.generateContent(prompt);
+
+  const response = result.response;
+  const text = response.text();
+  console.log(text);
+}
+
+run();
+
 // ---------- Firebase Setup ----------
 const firebaseConfig = {
   apiKey: "AIzaSyCq_XEdRwe1lRA7y2FfljFmfa5n-zQWJSw",
